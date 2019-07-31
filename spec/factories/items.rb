07d62d86eefc914599b683de1a61d6a331a928cc
Lucas Caponi da Silva <lukastsunami@MacBook-Pro-de-Lucas.local>
2019-07-31 05:40:54 -0300

    Creating item's factory and first test

---
+++ spec/factories/items.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+    factory :item, class: 'Item' do
+        name { Faker::Name.first_name }
+        points_value { Faker::Number.between(from: 1, to: 4) }
+        image_url { Faker::LoremPixel.image(size: "40x40") }
+    end
+end
