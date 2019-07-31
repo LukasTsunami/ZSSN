aaa9270468b2287a678cbbc696f9530a67b501e0
Lucas Caponi da Silva <lukastsunami@MacBook-Pro-de-Lucas.local>
2019-07-31 00:52:47 -0300

    Creating test to check Item creation

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe Item do
+  context 'When creating an item' do
+    context 'when data is valid' do
+      it 'should return a new item' do
+        new_item = create(:item)
+        expect(new_item).to be_valid
+      end
+    end
+  end
+end
