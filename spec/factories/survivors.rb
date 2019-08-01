29d092d441da5031f10b261866a37670765f0a51
Lucas Caponi da Silva <lukastsunami@MacBook-Pro-de-Lucas.local>
2019-08-01 01:50:50 -0300

    Creating survivor factory

---
+++ spec/factories/survivors.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :survivor, class: 'Survivor' do
+    name { Faker::Name.first_name }
+    age { Faker::Number.between(from: 16, to: 70) }
+    gender { Faker::Gender.binary_type }
+    latitude { Faker::Address.latitude  }
+    longitude { Faker::Address.longitude }
+    infected { :not_infected }
+    inventory { create(:items, Faker::Number.between(from: 16, to: 70)) }
+  end
+end
