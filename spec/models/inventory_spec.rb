4b792e45ed8c8015f42b3b70fbf6784983a1f5f7
Lucas Caponi da Silva <lukastsunami@MacBook-Pro-de-Lucas.local>
2019-08-02 00:43:11 -0300

    Creating test to if creating an inventory passing an instance of its references (item or survivor), it will assign it to its fields

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Inventory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'When creating an inventory' do
+    context 'passing a valid survivor' do
+      it 'should have the same survivor in inventory as the passed one' do
+        survivor = create(:survivor)
+        inventory = create(:inventory, survivor: survivor)
+        
+        expect(inventory.survivor).to equal(survivor)
+      end
+    end
+
+    context 'passing a valid item' do
+      it 'should have the same item in inventory as the passed one' do
+        item = create(:item)
+        inventory = create(:inventory, item: item)
+        
+        expect(inventory.item).to equal(item)
+      end
+    end
+  end
 end
