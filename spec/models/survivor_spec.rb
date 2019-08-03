40063fd0b1ba5a124e7f48d4e09d41c83e2c67c3
Lucas Caponi da Silva <lukastsunami@MacBook-Pro-de-Lucas.local>
2019-08-03 05:53:24 -0300

    Refactoring survivor's test to extract its creation to a let block

--- spec/models/survivor_spec.rb
+++ spec/models/survivor_spec.rb
@@ -3,18 +3,17 @@
 RSpec.describe Survivor do
   describe 'Creating a new survivor' do
     context 'when data is valid' do
+      let(:survivor) { create(:survivor, :with_inventory) }
+
       it 'should return a new survivor' do
-        survivor = create(:survivor)
         expect(survivor).to be_valid
       end
 
       it 'should return a new survivor with an inventory' do
-        survivor = create(:survivor, :with_inventory)
         expect(survivor.inventory).not_to be_nil
       end
 
       it 'should return a new survivor with an inventory, and direct access to its item list' do
-        survivor = create(:survivor, :with_inventory)
         expect(survivor.items).not_to be_nil
       end
     end
