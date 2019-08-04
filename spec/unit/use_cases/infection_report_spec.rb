6a86101496ab1421f043e4aae8c72199e05d7e1c
LukasTsunami <prateadow@gmail.com>
2019-08-04 01:31:18 -0300

    Testing for item's infection when its owner becames infected

---
+++ spec/unit/use_cases/infection_report_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'item'
+
+context 'When marking a survivor as infected' do
+  it 'should mark all its inventory items as infected too' do
+    survivor = create(:survivor, :with_inventory, infected: false)
+    survivor.infected = true
+    survivor.save
+
+    survivor.items.each do |item|
+    expect(item.infected?).to be_truthy
+    end
+  end
+end
