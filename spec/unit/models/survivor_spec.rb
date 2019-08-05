968e8f47e5ed780754fe75d0d4849f9326bc4c4e
LukasTsunami <prateadow@gmail.com>
2019-08-04 22:32:43 -0300

    Creating tests to validade which kind of values are suitable for survivors persistance

--- spec/unit/models/survivor_spec.rb
+++ spec/unit/models/survivor_spec.rb
@@ -3,17 +3,51 @@
 RSpec.describe Survivor, type: :model do
   describe 'Creating a new survivor' do
 
-    context "when validating presence" do
-      it { should validate_presence_of(:name) }
+    context 'when validating' do
+      context 'presence of' do
+        it { should validate_presence_of(:name) }
 
-      it { should validate_presence_of(:age) }
+        it { should validate_presence_of(:age) }
+  
+        it { should validate_presence_of(:latitude) }
+  
+        it { should validate_presence_of(:longitude) }
+  
+        it { should validate_presence_of(:gender) }
+      end
+     
+      context 'accepted_values' do
+        describe '#name' do
+          it { should_not allow_value(123).for(:name) }
+          it { should_not allow_value('almost@there.com').for(:name) }
+          it { should allow_value('Lucas').for(:name) }
+          it { should allow_value('Jon Tron').for(:name) }
+        end
 
-      it { should validate_presence_of(:latitude) }
+        describe '#age' do
+          it { should validate_inclusion_of(:age).in_range(10..100) }
+        end
 
-      it { should validate_presence_of(:longitude) }
+        describe '#gender' do
+          it { should allow_values(:male, :female).for(:gender) }
+        end
 
-      it { should validate_presence_of(:gender) }
+        describe '#latitude' do
+          it { should_not allow_value(123).for(:latitude) }
+          it { should_not allow_value('almost@there.com').for(:latitude) }
+          it { should allow_value(10.101245).for(:latitude) }
+          it { should validate_numericality_of(:latitude) }
+        end
+
+        describe '#longitude' do
+          it { should_not allow_value(123).for(:longitude) }
+          it { should_not allow_value('almost@there.com').for(:longitude) }
+          it { should allow_value(10.101245).for(:longitude) }
+          it { should validate_numericality_of(:longitude) }
+        end
+      end
     end
+
     
 
     context 'when data is valid' do
