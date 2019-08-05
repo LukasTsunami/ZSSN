6ff6457309430750120c85ddbcc722cb348c04df
LukasTsunami <prateadow@gmail.com>
2019-08-05 06:32:36 -0300

    Extracting item validation from model to a dedicated item validator

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,23 +5,5 @@ class Item < ApplicationRecord
   has_many :inventory
   has_many :survivors, through: :inventory
 
-  validates :name, presence: true, format: { with: /\A[a-z A-Z]+\z/, message: 'only allows letters' }
-  validates :points_value, presence: true, inclusion: { in: 1..4 }
-  validate :image_url?
-  validate :allowed_image_format?
-
-  def image_url?
-    return if image_url.nil?
-    return if image_url =~ /^#{URI::DEFAULT_PARSER.make_regexp}$/
-    return if allowed_image_format?
-
-    errors.add(:image_url, 'Please provide a valid image url')
-  end
-
-  def allowed_image_format?
-    return true if image_url.nil?
-    return true if image_url =~ /\.(png|jpg|gif|bmp|svg)\Z/i
-
-    errors.add(:image_url, 'Please provide image in one of this formats: png|jpg|gif|bmp|svg')
-  end
+  validates_with ItemValidator
 end
---
+++ app/models/validators/item_validator.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'active_model'
+
+# Extracts all validating business rules of Item
+class ItemValidator < SimpleDelegator
+  include ActiveModel::Validations
+
+  validates :name, presence: true, format: { with: /\A[a-z A-Z]+\z/, message: 'only allows letters' }
+  validates :points_value, presence: true, inclusion: { in: 1..4 }
+  validate :image_url?
+  validate :allowed_image_format?
+
+  # The most important thing to use validator as a separated class
+  def validate(item)
+    __setobj__(item)
+    super
+
+    item.errors.messages.merge!(errors.messages)
+  end
+
+  private
+
+  def image_url?
+    return if image_url.nil?
+    return if image_url =~ /^#{URI::DEFAULT_PARSER.make_regexp}$/
+    return if allowed_image_format?
+
+    errors.add(:image_url, 'Please provide a valid image url')
+  end
+
+  def allowed_image_format?
+    return true if image_url.nil?
+    return true if image_url =~ /\.(png|jpg|gif|bmp|svg)\Z/i
+
+    errors.add(:image_url, 'Please provide image in one of this formats: png|jpg|gif|bmp|svg')
+  end
+end
