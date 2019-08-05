03dbdb881f8303498ea83702838c69cb47348fb4
LukasTsunami <prateadow@gmail.com>
2019-08-05 05:44:01 -0300

    Creating tests to validate items persistance

--- spec/unit/models/item_spec.rb
+++ spec/unit/models/item_spec.rb
@@ -4,6 +4,34 @@ require 'item'
 
 RSpec.describe Item do
   context 'When creating an item' do
+    context 'when validating' do
+      describe '#name' do
+        it { should validate_presence_of(:name) }
+        it { should_not allow_value(123).for(:name) }
+        it { should_not allow_value('almost@there.com').for(:name) }
+        it { should allow_value('Lucas').for(:name) }
+        it { should allow_value('Jon Tron').for(:name) }
+      end
+
+      describe '#amount' do
+        it { should validate_presence_of(:amount) }
+        it { should validate_inclusion_of(:amount).in_range(0..999) }
+      end
+
+      describe '#points_value' do
+        it { should validate_presence_of(:points_value) }
+        it { should validate_inclusion_of(:points_value).in_range(1..4) }
+      end
+
+      describe '#image_url' do
+        it { should validate_presence_of(:image_url) }
+        it { should_not allow_value(123).for(:image_url) }
+        it { should_not allow_value('almost@there.com').for(:image_url) }
+        it { should allow_value('http://www.site.com/imagem.jpg').for(:image_url) }
+        it { should allow_value('./imagem.png').for(:image_url) }
+      end
+    end
+
     context 'when data is valid' do
       it 'should return a new item' do
         new_item = create(:item)
