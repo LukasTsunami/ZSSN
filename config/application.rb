f0f1f7d183fe4daed29084ddb08c2bf9356f1e79
LukasTsunami <prateadow@gmail.com>
2019-08-05 05:15:13 -0300

    Extracting survivor validation from model to a dedicated survivor validator

--- config/application.rb
+++ config/application.rb
@@ -23,15 +23,7 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    config.autoload_paths += [Rails.root.join('app', 'validators').to_s]
     config.api_only = true
     config.generators.test_framework :rspec
   end
